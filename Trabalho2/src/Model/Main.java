86aae0d872c4c8fc6b4627f42b4d2b72f48afcc8
Lorena Muralha <mlmuralha2301@gmail.com>
2023-05-22 00:22:33 -0300

    creating views

--- Trabalho2/src/Model/Main.java
+++ Trabalho2/src/Model/Main.java
@@ -1,9 +1,13 @@
 package Model;
 
+import View.MainFrame;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        MainFrame window = new MainFrame();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
     }
-    
+
 }
